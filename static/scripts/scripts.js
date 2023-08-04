27b2ff0ee9c90fb2a70d30c08e66e3596fd00048
Edwin Lambert <edwinrlambert@gmail.com>
2023-08-03 18:16:53 -0700

    Updated scripts for sending ajax requests to views

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -22,4 +22,42 @@ $(document).ready(function () {
         $("#div-media").show()
     });
 
+    // Form Submit and Show Overlay.
+    $("#btn-find-sentiment").click((e) => {
+        e.preventDefault();
+
+        let input = ""
+        if ($("#div-textarea").is(":visible")) {
+            input = $("#form-input-textarea").val();
+            type = "text";
+        } else if ($("#div-url").is(":visible")) {
+            input = $("#form-input-url").val();
+            type = "url";
+        } else if ($("#div-media").is(":visible")) {
+            input = $("#form-input-media").val();
+            type = "media";
+        }
+
+        if (input != '') {
+            $("#overlay").css("display", "block");
+            $.ajax({
+                type: "POST",
+                url: '/',
+                data: JSON.stringify({
+                    "input": input,
+                    "type": type
+                }),
+                dataType: 'json',
+                contentType: 'application/json'
+            })
+                .done((response) => {
+                    console.log(response)
+                    val_neg = Math.round(parseFloat(response["score_negative"]) * 10000) / 100
+                    val_neu = Math.round(parseFloat(response["score_neutral"]) * 10000) / 100
+                    val_pos = Math.round(parseFloat(response["score_positive"]) * 10000) / 100
+                })
+        }
+
+    })
+
 })
